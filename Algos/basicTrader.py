54adc4f7d12e2d39018e0fb1392f42a3e9582ecb
Andrew Wang <44416801+aywang71@users.noreply.github.com>
2024-04-10 21:36:49 -0400

    update for trader algo

--- Algos/basicTrader.py
+++ Algos/basicTrader.py
@@ -21,36 +21,106 @@ class Trader:
         return midpoint
     def run(self, state: TradingState):
         print("traderData: " + state.traderData)
+        #TODO: Cannot hardcode position values, must paramterize as # products increases
+        ame = state.position.get("AMETHYSTS", 0)
+        ameLimit = 20
+        star = state.position.get("STARFRUIT", 0)
+        starLimit = 20
+        print("positions: " + str(ame) + ", " + str(star))
         print("Observations: " + str(state.observations))
 
         # Orders to be placed on exchange matching engine
         result = {}
         for product in state.order_depths:
-            order_depth: OrderDepth = state.order_depths[product]
-            orders: List[Order] = []
-            #Calculate acceptable price as the midpoint between the medians of buy/sell
-            acceptable_price = self.calcPrice(order_depth)
-            print("Acceptable price : " + str(acceptable_price))
-            print("Buy Order depth : " + str(len(order_depth.buy_orders)) + ", Sell order depth : " + str(
-                len(order_depth.sell_orders)))
+            # TODO: Cannot hardcode position values, need to paramterize them
+            if product == "AMETHYSTS": #high-freqency
+                order_depth: OrderDepth = state.order_depths[product]
+                orders: List[Order] = []
+                # Always midpoint at 10k due to stability
+                acceptable_price = 10000
+                print("Buy Order depth : " + str(len(order_depth.buy_orders)) + ", Sell order depth : " + str(
+                    len(order_depth.sell_orders)))
 
-            if len(order_depth.sell_orders) != 0:
-                best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
-                if int(best_ask) < acceptable_price:
-                    print("BUY", str(-best_ask_amount) + "x", best_ask)
-                    orders.append(Order(product, best_ask, -best_ask_amount))
+                i = 0
+                while i < len(order_depth.sell_orders):
+                    ask, ask_amount = list(order_depth.sell_orders.items())[i]
+                    ask_amount = min(ask_amount, ameLimit - ame)
+                    if int(ask) <= acceptable_price:
+                        print("BUY", str(-ask_amount) + "x", ask)
+                        orders.append(Order(product, ask, -ask_amount))
+                        ame += ask_amount
+                    i += 1
 
-            if len(order_depth.buy_orders) != 0:
-                best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
-                if int(best_bid) > acceptable_price:
-                    print("SELL", str(best_bid_amount) + "x", best_bid)
-                    orders.append(Order(product, best_bid, -best_bid_amount))
+                """if len(order_depth.sell_orders) != 0:
+                    best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
+                    if int(best_ask) <= acceptable_price:
+                        print("BUY", str(-best_ask_amount) + "x", best_ask)
+                        orders.append(Order(product, best_ask, -best_ask_amount))"""
+                ame = state.position.get("AMETHYSTS", 0)
+                i = 0
+                while i < len(order_depth.sell_orders):
+                    bid, bid_amount = list(order_depth.buy_orders.items())[0]
+                    bid_amount = min(-bid_amount, ame + ameLimit)
+                    if int(bid) >= acceptable_price:
+                        print("SELL", str(bid_amount) + "x", bid)
+                        orders.append(Order(product, bid, -bid_amount))
+                        ame -= bid_amount
+                    i += 1
+                """if len(order_depth.buy_orders) != 0:
+                    best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
+                    if int(best_bid) >= acceptable_price:
+                        print("SELL", str(best_bid_amount) + "x", best_bid)
+                        orders.append(Order(product, best_bid, -best_bid_amount))"""
 
-            result[product] = orders
+                result[product] = orders
+            else:
+                order_depth: OrderDepth = state.order_depths[product]
+                orders: List[Order] = []
+                #Calculate acceptable price as the midpoint between the medians of buy/sell
+                acceptable_price = self.calcPrice(order_depth)
+                print("Acceptable price : " + str(acceptable_price))
+                print("Buy Order depth : " + str(len(order_depth.buy_orders)) + ", Sell order depth : " + str(
+                    len(order_depth.sell_orders)))
 
-            # String value holding Trader state data required.
+                i = 0
+                while i < len(order_depth.sell_orders):
+                    ask, ask_amount = list(order_depth.sell_orders.items())[i]
+                    ask_amount = min(-ask_amount, starLimit - star)
+                    if int(ask) <= acceptable_price:
+                        print("BUY", str(-ask_amount) + "x", ask)
+                        orders.append(Order(product, ask, -ask_amount))
+                        star += ask_amount
+                    i += 1
+
+                """if len(order_depth.sell_orders) != 0:
+                    best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
+                    if int(best_ask) < acceptable_price:
+                        print("BUY", str(-best_ask_amount) + "x", best_ask)
+                        orders.append(Order(product, best_ask, -best_ask_amount))"""
+                star = state.position.get("STARFRUIT", 0)
+
+                i = 0
+                while i < len(order_depth.sell_orders):
+                    bid, bid_amount = list(order_depth.buy_orders.items())[0]
+                    bid_amount = min(bid_amount, star + starLimit)
+                    if int(bid) >= acceptable_price:
+                        print("SELL", str(bid_amount) + "x", bid)
+                        orders.append(Order(product, bid, -bid_amount))
+                        star -= bid_amount
+                    i += 1
+                #TODO: Put in an order for whatever is remaining on buy/sell limits to buy at very good price and see if anyone matches it
+                """if len(order_depth.buy_orders) != 0:
+                    best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
+                    if int(best_bid) > acceptable_price:
+                        print("SELL", str(best_bid_amount) + "x", best_bid)
+                        orders.append(Order(product, best_bid, -best_bid_amount))"""
+
+                result[product] = orders
+
+        # String value holding Trader state data required.
         # It will be delivered as TradingState.traderData on next execution.
-        traderData = "SAMPLE" #TODO: Change to TradingState.traderData
+        traderData = state.traderData
+        print(result)
 
         # Sample conversion request. Check more details below.
         conversions = 1
